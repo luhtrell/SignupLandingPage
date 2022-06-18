8b6417ea7969a5967533932795de4e9da4200760
Luh Trell <sanchezlatrell30@gmail.com>
2022-06-18 12:22:39 -0400

    script for first name error

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-function printError(elemId, hintMsg) {
-    document.getElementById(elemId).innerHTML = hintMsg;
-}
+const firstName = document.getElementById('firstName');
+const form = document.getElementById('form');
+const button = document.getElementById('button');
+const errorElement = document.getElementById('firstErr')
 
-function validateForm() {
-    var firstName = document.signup.first.value;
-    var lastName = document.signup.last.value;
-    var email = document.signup.email.value;
-    var password = document.signup.pass.value;
-}
+form.addEventListener("submit", (e) => {
+    let messages = []
+    if(firstName.value === '' || firstName.value == null) {
+        messages.push('First Name cannot be empty')
+    }
 
-var firstErr = lastErr = emailErr = passErr = true;
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+    e.preventDefault()
+})
 
-if(firstName==null || firstName=="") {
-    alert("Name can't be blank");
-    return false;
-}
+// function validateForm() {
+//     var firstName = document.signup.first.value;
+//     var lastName = document.signup.last.value;
+//     var email = document.signup.email.value;
+//     var password = document.signup.pass.value;
+// }
+
+// var firstErr = lastErr = emailErr = passErr = true;
+
+// if(firstName==null || firstName=="") {
+//     alert("Name can't be blank");
+//     return false;
+// }
