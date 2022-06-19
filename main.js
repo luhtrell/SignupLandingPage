56ad4d546accbdff3b9b60e311e9c08b97f55d6a
Luh Trell <sanchezlatrell30@gmail.com>
2022-06-19 13:02:09 -0400

    completed errors for first and second inputs

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName')
 const form = document.getElementById('form');
 const button = document.getElementById('button');
-const errorElement = document.getElementById('firstErr')
+const errorElementFirst = document.getElementById('firstErr')
+const errorElementSecond = document.getElementById('lastErr')
 
-form.addEventListener("submit", (e) => {
-    let messages = []
-    if(firstName.value === '' || firstName.value == null) {
-        messages.push('First Name cannot be empty')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const firstNamevalue = firstName.value.trim();
+    const lastNamevalue = lastName.value.trim();
+
+    if(firstNamevalue === '') {
+        errorElementFirst.innerText = 'First Name cannot be empty';
     }
 
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-    e.preventDefault()
-})
+    if(lastNamevalue === '') {
+        errorElementSecond.innerText = 'Last Name cannot be empty';
+    } 
+}
+
+// function setErrorFor(input,message) {
+//     errorElementFirst.innerText = message;
+// }
+
+// function setSuccessFor(input) {
+    
+// }
+
+
+// form.addEventListener("submit", (e) => {
+//     let messages = []
+//     if(firstName.value === '' || firstName.value == null) {
+//         messages.push('First Name cannot be empty')
+//     }
+
+//     if (messages.length > 0) {
+//         e.preventDefault()
+//         errorElementFirst.innerText = messages.join(', ')
+//     }
+//     e.preventDefault()
+// })
 
 // function validateForm() {
 //     var firstName = document.signup.first.value;
